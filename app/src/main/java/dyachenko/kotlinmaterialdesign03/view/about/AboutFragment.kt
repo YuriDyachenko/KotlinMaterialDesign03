ca8485dbdb5fea20bb0e42bdfc447ac4431f534a
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-08-21 21:37:25 +0300

    шрифт из assets применил к текстВью на экране "о НАСА", а из папки font через тему ко всему приложению. но на chips не подействовало, даже если в их стиле добавлял fontfamily

--- app/src/main/java/dyachenko/kotlinmaterialdesign03/view/about/AboutFragment.kt
+++ app/src/main/java/dyachenko/kotlinmaterialdesign03/view/about/AboutFragment.kt
@@ -1,7 +1,9 @@
 package dyachenko.kotlinmaterialdesign03.view.about
 
+import android.graphics.Typeface
 import android.os.Bundle
 import android.view.*
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import dyachenko.kotlinmaterialdesign03.R
 import dyachenko.kotlinmaterialdesign03.util.hideAllItems
@@ -20,6 +22,11 @@ class AboutFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setHasOptionsMenu(true)
+
+        activity?.let {
+            view.findViewById<TextView>(R.id.about_text_view).typeface =
+                Typeface.createFromAsset(it.assets, "fonts/CuteAurora-PK3lZ.ttf")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
