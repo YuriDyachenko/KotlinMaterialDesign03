86ba3b7bd95f0a5d2761b4fdafb1561b1fbd81f9
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-08-22 09:00:35 +0300

    запуск MainActivity по завершении анимации, добавил и интерполятор, но особой разницы не заметил

--- app/src/main/java/dyachenko/kotlinmaterialdesign03/view/splash/SplashActivity.kt
+++ app/src/main/java/dyachenko/kotlinmaterialdesign03/view/splash/SplashActivity.kt
@@ -1,9 +1,9 @@
 package dyachenko.kotlinmaterialdesign03.view.splash
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import dyachenko.kotlinmaterialdesign03.R
@@ -11,23 +11,27 @@ import dyachenko.kotlinmaterialdesign03.view.main.MainActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    private val handler = Handler(Looper.getMainLooper())
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         val image = findViewById<ImageView>(R.id.splash_image_view)
-        image.animate().rotationY(360F).setDuration(3000).start()
+        image
+            .animate()
+            .rotationY(360F)
+            .setInterpolator(AccelerateDecelerateInterpolator())
+            .setDuration(2000)
+            .setListener(object : Animator.AnimatorListener {
 
-        handler.postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, 3000)
-    }
+                override fun onAnimationEnd(animation: Animator?) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
 
-    override fun onDestroy() {
-        handler.removeCallbacksAndMessages(null)
-        super.onDestroy()
+                override fun onAnimationStart(animation: Animator?) {}
+                override fun onAnimationCancel(animation: Animator?) {}
+                override fun onAnimationRepeat(animation: Animator?) {}
+            })
+            .start()
     }
 }
